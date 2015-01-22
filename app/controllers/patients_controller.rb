daabc405024c4fec0365a64b1a2f85247864d428
Travis Nesland <tnesland@gmail.com>
2015-01-21 20:47:17 -0500

    Build CRUD for patient records.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,64 @@
+class PatientsController < ApplicationController
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find params[:id]
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      flash[:notice] = 'Patient was created succesfully'
+      redirect_to patients_path
+    else
+      flash[:alert] = 'There was a problem: Patient was not saved!'
+      render :new
+    end
+  end
+
+  def edit
+    @patient = Patient.find params[:id]
+  end
+
+  def update
+    @patient = Patient.find params[:id]
+    if @patient.update(patient_params)
+      flash[:notice] = 'Patient succesfully updated'
+      redirect_to patients_path
+    else
+      flash[:alert] = 'There was a problem: Patient could not be updated!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @patient = Patient.find params[:id]
+    if @patient.destroy
+      flash[:notice] = 'Patient was succesfully removed'
+      redirect_to patients_path
+    else
+      flash[:alert] = 'There was a problem: Patient could not be removed!'
+      redirect_to patients_path
+    end
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:name_first,
+                                    :name_last,
+                                    :date_of_birth,
+                                    :description,
+                                    :gender,
+                                    :blood_type
+                                   )
+    
+  end
+  
+end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,27 @@
 class Patient < ActiveRecord::Base
+
+  BLOOD_TYPE_OPTIONS = ['O+','O-','A+','A-',
+                        'B+','B-','AB+','AB-']
+  GENDER_OPTIONS = ['M','F']
+
+  validates :name_first, :name_last, presence: true
+  validate :date_of_birth_at_least_ten_years_ago
+  validates_inclusion_of :gender, in: GENDER_OPTIONS
+  validates_inclusion_of :blood_type, in: BLOOD_TYPE_OPTIONS
+
+  def date_of_birth_at_least_ten_years_ago
+    ten_years_ago = Date.today << 12*10
+    if self.date_of_birth > ten_years_ago
+      errors.add(:date_of_birth, "must be on or before #{ten_years_ago}")
+    end
+  end
+  
+  def isFemale?
+    return self.gender == 'F'
+  end
+
+  def isMale?
+    return self.gender == 'M'
+  end
+  
 end
