bd2c5423cb3d6ab5f856a17e65f45e2e5ab30a47
Travis Nesland <tnesland@gmail.com>
2015-01-22 12:54:43 -0500

    Create hospital model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121180626) do
+ActiveRecord::Schema.define(version: 20150122174121) do
+
+  create_table "hospitals", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "name_first"
