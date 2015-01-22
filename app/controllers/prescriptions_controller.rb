81ad05b62cc6175634b0aa758476f5aac005e1b8
Travis Nesland <tnesland@gmail.com>
2015-01-22 16:27:56 -0500

    Add views and controlling logic for prescriptions.

---
+++ app/controllers/prescriptions_controller.rb
@@ -0,0 +1,55 @@
+class PrescriptionsController < ApplicationController
+
+  def show
+    @prescription = Prescription.find params[:id]
+    @patient = @prescription.patient
+  end
+  
+  def new
+    @patient = Patient.find params[:patient_id]
+    @prescription = @patient.prescriptions.new
+  end
+
+  def create
+    @patient = Patient.find params[:patient_id]
+    @prescription = @patient.prescriptions.create prescription_params
+    redirect_to patient_path(@patient)
+  end
+
+  def edit
+    @prescription = Prescription.find params[:id]
+    @patient = @prescription.patient
+  end
+
+  def update
+    @prescription = Prescription.find params[:id]
+    @patient = @prescription.patient
+    if @prescription.update prescription_params
+      flash[:notice] = 'Prescription was successfully saved'
+      redirect_to patient_path(@patient)
+    else
+      flash[:alert] = 'There was a problem: Prescription could not be saved!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @prescription = Prescription.find params[:id]
+    @patient = @prescription.patient
+    if @prescription.destroy
+      flash[:notice] = 'Prescription was deleted'
+      redirect_to patient_path(@patient)
+    else
+      flash[:alert] = 'There was a problem: Prescription could not be deleted!'
+      redirect_to patient_path(@patient)
+    end
+  end
+  
+  private
+  def prescription_params
+    params.require(:prescription).permit(
+                                         :medication
+                                        )
+  end
+  
+end
