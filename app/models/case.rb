833af31f79b6beed740384ce959411719e7103ce
Travis Nesland <tnesland@gmail.com>
2015-01-28 14:57:00 -0500

    Fix typo in case - notes relationship.

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,5 +2,5 @@ class Case < ActiveRecord::Base
   belongs_to :patient
   has_many :case_symptoms
   has_many :symptoms, through: :case_symptoms
-  has_many :notes, through: :noteable
+  has_many :notes, as: :noteable
 end
