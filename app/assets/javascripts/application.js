142ce22a84074cbb2ca45ebde4d599a2576573e9
Travis Nesland <tnesland@gmail.com>
2015-02-04 17:01:52 -0500

    Fade-in submit button if form is correct.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,46 @@
 
 // function to fire error after leaving a required field empty
 required_field_check = function() {
+    var parent_div = $(this).closest('.form-group');    
     if ($(this).val() == '') {
-        var parent_div = $(this).closest('.form-group');
-        parent_div.addClass('error');
+        parent_div.attr('valid', false)
         parent_div.find('.error-message').removeClass('hidden');
+    } else {
+        parent_div.attr('valid', true)        
+        parent_div.find('.error-message').addClass('hidden');
     }
+}
+
+// a form is valid if all required inputs have a value
+form_is_valid = function () {
+    // default return value is true
+    var is_valid = true;
+    // get the input for all required fields
+    var $required_fields = $('form .required :input');
+    // for each required field
+    $required_fields.each( function() {
+        $parent_div = $( this ).closest('.form-group');
+        if ($( this ).val() == '') {
+            is_valid = false;
+            $parent_div.attr('valid', false);
+        } else {
+            $parent_div.attr('valid', true);
+        }
+    })
+    return is_valid;
 }
 
 // register some stuff once the document is ready to go
 $(document).ready( function() {
+    // hide any submit buttons
+    $('form').find('[type="submit"]').hide();
     $('.form-group.required input').bind('focusout',required_field_check);
     $('.form-group.required select').bind('focusout',required_field_check);
     $('.form-group.required radio').bind('focusout',required_field_check);
+    // fade in submit button if the form is valid
+    $('form').change( function() {
+        if (form_is_valid()) {
+            $(this).find('[type="submit"]').fadeIn()
+        }
+    })
 })
